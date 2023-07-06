6382c92e02a8b04da2c8a259d1af579eaf69a279
pentaputri25 <138760712+pentaputri25@users.noreply.github.com>
2023-07-06 14:25:40 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+def main():
+	st.title("BIOGRAPHY")
+	st.image("foto.jpg")
+	st.header("PENTA PUTRI NUNIK YUNIARTI")
+
+	st.subheader("Programer Kesehatan")
+	st.write("Sebagai progamer dalam lingkup pengkodingan data riwayat pasien dalam skala puskesmas ataupun rumah sakit")
+	st.subheader("Biodata Diri")
+	st.write("Tempat Tanggal Lahir : Grobogan, 25-06-2003")
+	st.write("Alamat : Jalan Kartini No 11")
+	st.write("No Telf : 08567845543")
+	st.write("Agama : Kristen")
+
+	st.subheader("Pendidikan")
+	st.write("2009-2010 : TK Kristen Purwodadi")
+	st.write("2010-2016 : SD Kristen Purwodadi")
+	st.write("2016-2019 : SMP 1 Purwodadi")
+	st.write("2019-2022 : SMK Theresiana")
+	st.write("2022-2025 : Universitas Nasional Karangturi _prodi Manajemen Informasi Kesehatan_")
+	
+	st.subheader("Kualifikasi")
+	st.write("Kemampuan : Mengerti MS Word Ms Excel dan mengenai pengkodingan")
+	st.write("Kemampuan : Mengelola Data Rekam Medis")
+	st.write("Pengalaman Kerja : Januari 2023-Juli 2023 Di Rumah Sakit Tulip")
+
+
+if __name__=="__main__":
+	main();
